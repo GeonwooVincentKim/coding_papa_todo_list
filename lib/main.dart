4f6d6c607974988e619e1cf155104e2e57e9931c
GeonwooVincentKim <kdsnop@gmail.com>
2023-02-25 23:48:20 +0900

    [Feature] - Updated codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:coding_papa_flutter_todo_list/old_task.dart';
+import 'package:coding_papa_flutter_todo_list/task_tile.dart';
 import 'package:flutter/material.dart';
 
 final List<Task> _items = [];
@@ -87,75 +88,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             _items.insert(newIndex, item);
           });
         },
-      ),
-    );
-  }
-}
-
-class TaskTile extends StatefulWidget {
-  TaskTile({
-    Key? key,
-    required this.itemIndex,
-    required this.onDeleted,
-  }) : super(key: key);
-
-  final int itemIndex;
-  final Function onDeleted;
-
-  @override
-  State<TaskTile> createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  @override
-  Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color evenItemColor = colorScheme.primary;
-    final Task item = _items[widget.itemIndex];
-
-    return Material(
-      child: AnimatedContainer(
-        constraints: const BoxConstraints(minHeight: 60),
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: item.finished ? Colors.grey : evenItemColor,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.fastOutSlowIn,
-        child: Row(
-          children: [
-            Checkbox(
-              key: widget.key,
-              value: item.finished,
-              onChanged: (checked) {
-                setState(() {
-                  item.finished = checked ?? false;
-                });
-              },
-            ),
-            Expanded(
-              child: Text(
-                item.title,
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white,
-                  decoration: item.finished
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-              onPressed: () => widget.onDeleted(),
-            )
-          ],
-        ),
       ),
     );
   }
